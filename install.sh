1179545bd2815a010f5854325a87536607bc5319
Basti Tee <basti.tee@posteo.de>
2017-09-12 09:12:04 +0200

    bundled with 0.1.0-pre release of robota for interface stability;

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 cd "$( dirname "$( readlink -f $0 )" )"
 robota_dir="_robota_install"
+robota_ver="0.1.0"
 
-[ ! -d $robota_dir ] && git clone https://github.com/BastiTee/bastis-data-analysis-toolbox.git $robota_dir
+[ -d $robota_dir ] && exit 0
+wget https://github.com/BastiTee/robota/archive/${robota_ver}.zip
+unzip ${robota_ver}.zip 
+mv robota-${robota_ver} $robota_dir
 chmod a+x ${robota_dir}/dev-tools/install.sh
 ${robota_dir}/dev-tools/install.sh
+rm -f ${robota_ver}.zip
